7e3fbd408097185da07ad2fe0f1754d0f8f59581
axlod <axl.orellana@tipti.market>
2024-02-02 01:24:23 -0500

    set app bar theme values

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,11 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
         scaffoldBackgroundColor: const Color(0xff35654d),
-        appBarTheme: const AppBarTheme(backgroundColor: Color(0xff35654d)),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xff35654d),
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.white),
+        ),
       ),
       initialRoute: HomePage.routeName,
       routes: {
